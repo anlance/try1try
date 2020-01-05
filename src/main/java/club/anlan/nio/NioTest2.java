62990a8354edc12040eb844900251a428c1b87dc
anlance <anlan1996@qq.com>
2020-01-05 19:35:29 +0800

    NioTest2

---
+++ src/main/java/club/anlan/nio/NioTest2.java
@@ -0,0 +1,24 @@
+package club.anlan.nio;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+
+public class NioTest2 {
+    public static void main(String[] args) throws IOException {
+        FileInputStream fileInputStream = new FileInputStream("src/NioTest2.txt");
+        FileChannel channel = fileInputStream.getChannel();
+
+        ByteBuffer byteBuffer = ByteBuffer.allocate(512);
+        channel.read(byteBuffer);
+
+        byteBuffer.flip();
+
+        while (byteBuffer.hasRemaining()){
+            byte b = byteBuffer.get();
+            System.out.println((char) b);
+        }
+        fileInputStream.close();
+    }
+}
