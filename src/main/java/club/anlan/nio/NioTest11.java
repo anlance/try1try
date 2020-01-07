d36537f0009206c8390132d7ad7947c9b9a17c66
anlance <anlan1996@qq.com>
2020-01-07 19:14:44 +0800

    NioTest 11&12

---
+++ src/main/java/club/anlan/nio/NioTest11.java
@@ -0,0 +1,47 @@
+package club.anlan.nio;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.util.Arrays;
+
+public class NioTest11 {
+    public static void main(String[] args) throws IOException {
+        ServerSocketChannel serverSocketChannel = ServerSocketChannel.open();
+        InetSocketAddress address = new InetSocketAddress(8899);
+        serverSocketChannel.socket().bind(address);
+
+        int messageLength = 2 + 3 + 4;
+        ByteBuffer[] buffers = new ByteBuffer[3];
+        buffers[0] = ByteBuffer.allocate(2);
+        buffers[1] = ByteBuffer.allocate(3);
+        buffers[2] = ByteBuffer.allocate(4);
+
+        SocketChannel socketChannel = serverSocketChannel.accept();
+
+        while (true) {
+            int bytesRead = 0;
+            while (bytesRead < messageLength) {
+                long r = socketChannel.read(buffers);
+                bytesRead += r;
+                System.out.println("bytesRead: "+bytesRead);
+
+                Arrays.stream(buffers).
+                        map(buffer->"position: "+buffer.position()+", limit: "+buffer.limit()).
+                        forEach(System.out::println);
+            }
+
+            Arrays.stream(buffers).forEach(buffer->buffer.flip());
+
+            long bytesWritten = 0;
+            while (bytesWritten<messageLength){
+                long r = socketChannel.write(buffers);
+                bytesWritten+=r;
+            }
+
+            Arrays.stream(buffers).forEach(buffer->buffer.clear());
+        }
+    }
+}
