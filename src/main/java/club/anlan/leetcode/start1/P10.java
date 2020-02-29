94439ac31e90befe063a39cf3b71f406e65a0ffe
anlan <anlan1996@qq.com>
2020-03-01 01:57:57 +0800

    9

---
+++ src/main/java/club/anlan/leetcode/start1/P10.java
@@ -0,0 +1,58 @@
+package club.anlan.leetcode.start1;
+
+public class P10 {
+    public static void main(String[] args) {
+        P10 p = new P10();
+        String ss = "ab";
+        String pp = ".*";
+        System.out.println(p.isMatch(ss, pp));
+    }
+
+    public boolean isMatchOne(String s, String p, int i, int j) {
+        if (i == s.length() && j==p.length())
+            return true;
+        // 必须先判断特殊情况 ，比如 s 和 p 都是 *
+        if (i < s.length() && j < p.length()) {
+            if (p.charAt(j) == '.') { // . 匹配当前位置
+                if(j+1<p.length() && p.charAt(j+1)=='*')
+                    return isMatchOne(s,p,i,j+2) || isMatchOne(s, p, i + 1, j + 1); // 同时去掉当前字符和下一个*符号
+                return isMatchOne(s, p, i + 1, j + 1);
+            } else if (p.charAt(j) == '*') { // * 匹配当前位置 或者不匹配
+                // 第一个为 * 时只能空匹配
+                if (i == 0)
+                    return isMatchOne(s, p, i, j + 1);
+                // * 匹配一次或者多次
+                return (isMatchOne(s, p, i + 1, j + 1) && (p.charAt(j-1)=='.' || p.charAt(j-1)==s.charAt(i))) || isMatchOne(s, p, i+1, j);
+            } else if (s.charAt(i) == p.charAt(j))
+                return isMatchOne(s, p, i + 1, j + 1);
+            else{ // 不同时，下一个必须是*来 0 重复才有可能匹配（* 匹配 0 次）
+                if(j+1<p.length() && p.charAt(j+1)=='*')
+                    return isMatchOne(s,p,i,j+2); // 同时去掉当前字符和下一个*符号
+                return false;
+            }
+        } else if (j < p.length()) {
+            while(j<p.length() && p.charAt(j)=='*'){
+                ++j;
+            }
+            return j==p.length();
+        }
+        // p 已经匹配完而 s 还有未匹配
+        return false;
+    }
+
+    // 递归来处理（主要是 * 可以匹配可以不匹配）
+    public boolean isMatch(String s, String p) {
+        if (s.isEmpty() || p.isEmpty()) {
+            if (s.isEmpty()) {
+                for (int i = 0; i < p.length(); ++i) {
+                    if (p.charAt(i) != '*')
+                        return false;
+                }
+                return true;
+            }
+            // s 为空 ， p不为空
+            return false;
+        }
+        return isMatchOne(s, p, 0, 0);
+    }
+}
