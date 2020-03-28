97d689ed9b43f604f47605abe39f0650bf52f3cc
anlan <anlan1996@qq.com>
2020-03-28 14:02:21 +0800

    65(regex) && 66

---
+++ src/main/java/club/anlan/leetcode/start61/P65.java
@@ -0,0 +1,16 @@
+package club.anlan.leetcode.start61;
+
+import java.util.regex.Pattern;
+
+public class P65 {
+    public static void main(String[] args) {
+        P65 p = new P65();
+        System.out.println(p.isNumber("-90e3"));
+    }
+
+    public boolean isNumber(String s) {
+        String p1 = "[+-]?(([0-9]+\\.?[0-9]*)|([0-9]*\\.?[0-9]+))(e[+-]?[0-9]+)?";
+        s = s.trim();
+        return Pattern.matches(p1, s);
+    }
+}
---
+++ src/main/java/club/anlan/leetcode/start61/P66.java
@@ -0,0 +1,31 @@
+package club.anlan.leetcode.start61;
+
+import java.util.Arrays;
+
+public class P66 {
+    public static void main(String[] args) {
+        P66 p = new P66();
+        int[] dig = {9,9,9};
+        int[] res = p.plusOne(dig);
+        Arrays.stream(res).forEach(i-> System.out.print(i+" "));
+    }
+
+    public int[] plusOne(int[] digits) {
+        if (digits[digits.length - 1] != 9) {
+            digits[digits.length - 1] += 1;
+            return digits;
+        }
+        int[] res = new int[digits.length + 1];
+        for (int j = digits.length - 1; j >= 0; --j) {
+            if (digits[j] == 9) {
+                digits[j] = 0;
+                res[j + 1] = 0;
+            } else {
+                digits[j] += 1;
+                return digits;
+            }
+        }
+        res[0] = 1;
+        return res;
+    }
+}
