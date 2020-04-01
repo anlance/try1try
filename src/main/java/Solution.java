321c7f654a9bb0c6471c4243e40d5bbf3820b421
anlan <anlan1996@qq.com>
2020-04-01 13:03:49 +0800

    73(字符串) && 74(dp)

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 public class Solution {
     public static void main(String[] args) {
@@ -10,38 +7,33 @@ public class Solution {
         while (t > 0) {
             --t;
             int n = sc.nextInt();
-            String str = sc.next();
-            char[] a = new char[n];
-            Arrays.fill(a, '0');
-            a[0] = '1';
-            char[] b = new char[n];
-            Arrays.fill(b, '0');
-            b[0] = '1';
-            boolean isSame = true;
-            int index = n;
-            for (int i = 1; i < n; ++i) {
-                if (str.charAt(i) == '0')
-                    continue;
-                if (str.charAt(i) == '2') {
-                    a[i] = '1';
-                    b[i] = '1';
-                } else {
-                    a[i] = '1';
-                    index = i;
-                    break;
-                }
-            }
-            for (int i = index + 1; i < n; ++i) {
-                b[i] = str.charAt(i);
-            }
-            StringBuilder aa = new StringBuilder();
-            StringBuilder bb = new StringBuilder();
+            int[] number = new int[n];
+            int[] res = new int[n];
+            Arrays.fill(res,0);
+            boolean[] used = new boolean[n];
             for (int i = 0; i < n; ++i) {
-                aa.append(a[i]);
-                bb.append(b[i]);
+                number[i] = sc.nextInt();
             }
-            System.out.println(aa);
-            System.out.println(bb);
+            List<Integer> list = new LinkedList<>();
+            int index = 1;
+            res[0]=1;
+            list.add(0);
+            int closed = n*n;
+
+            for(int i=0;i<res.length;++i){
+                System.out.print(res[i]+" ");
+            }
+            System.out.println();
         }
+    }
+
+    public static int gcd(int a, int b) {
+        int r;
+        while (b > 0) {
+            r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
     }
 }
