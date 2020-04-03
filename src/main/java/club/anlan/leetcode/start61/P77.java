b9fca7d0fcb42a15507633b47521da8b671dbc62
anlan <anlan1996@qq.com>
2020-04-03 13:56:34 +0800

    77(backtracking) && 78(backtracking)

---
+++ src/main/java/club/anlan/leetcode/start61/P77.java
@@ -0,0 +1,32 @@
+package club.anlan.leetcode.start61;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P77 {
+    public static void main(String[] args) {
+        P77 p = new P77();
+        List<List<Integer>> res = p.combine(6, 4);
+        res.forEach(i -> i.forEach(j -> System.out.print(j + "")));
+    }
+
+
+    public List<List<Integer>> combine(int n, int k) {
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(n, 1, k, list, res);
+        return res;
+    }
+
+    public void dfs(int n, int i, int k, List<Integer> list, List<List<Integer>> res) {
+        if (k == 0) {
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for (int j = i; j <= n - k + 1; ++j) {
+            list.add(j);
+            dfs(n, j + 1, k - 1, list, res);
+            list.remove(list.size() - 1);
+        }
+    }
+}
---
+++ src/main/java/club/anlan/leetcode/start61/P78.java
@@ -0,0 +1,35 @@
+package club.anlan.leetcode.start61;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P78 {
+    public static void main(String[] args) {
+        P78 p = new P78();
+        int[] nums = {1,2,3};
+        List<List<Integer>> res = p.subsets(nums);
+        res.forEach(li-> System.out.println(li.toString()));
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        List<List<Integer>> res = new ArrayList<>();
+        res.add(new ArrayList<>());
+        for(int i=1;i<nums.length+1;++i){
+            dfs(i,0,nums,list,res);
+        }
+        return res;
+    }
+
+    public void dfs(int n, int i,int[] nums,List<Integer> list,List<List<Integer>> res){
+        if(n==0){
+            res.add(new ArrayList<>(list));
+            return;
+        }
+        for(int j=i;j<nums.length;++j){
+            list.add(nums[j]);
+            dfs(n-1,j+1,nums,list,res);
+            list.remove(list.size()-1);
+        }
+    }
+}
