c33a3285d5ef0914961a4aa099b7a42969695e1c
anlan <anlan1996@qq.com>
2020-04-09 13:48:47 +0800

    89 && 90

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -3,37 +3,28 @@ import java.util.*;
 public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int t = sc.nextInt();
-        while (t > 0) {
-            --t;
-            int n = sc.nextInt();
-            int[] number = new int[n];
-            int[] res = new int[n];
-            Arrays.fill(res,0);
-            boolean[] used = new boolean[n];
-            for (int i = 0; i < n; ++i) {
-                number[i] = sc.nextInt();
-            }
-            List<Integer> list = new LinkedList<>();
-            int index = 1;
-            res[0]=1;
-            list.add(0);
-            int closed = n*n;
-
-            for(int i=0;i<res.length;++i){
-                System.out.print(res[i]+" ");
-            }
-            System.out.println();
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        int count = (1 + n) * n / 2;
+        for (int i = 0; i < n; ++i) {
+            num[i] = sc.nextInt();
         }
+        int index = 0;
+        int temp = 0;
+        while (index < n) {
+            long sum = 0;
+            for (int i = index; i < n; ++i) {
+                sum += num[i];
+                if (sum == 0) {
+                    count -= (n - i) * (index - temp + 1);
+                    temp = index + 1;
+                    break;
+                }
+            }
+            ++index;
+        }
+        System.out.println(count);
     }
 
-    public static int gcd(int a, int b) {
-        int r;
-        while (b > 0) {
-            r = a % b;
-            a = b;
-            b = r;
-        }
-        return a;
-    }
+
 }
